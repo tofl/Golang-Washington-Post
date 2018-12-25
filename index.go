737b853320d6275a27f2206c71eab0d5fe4b0252
Tom F <thomas.flitterman@ynov.com>
2018-12-26 00:29:49 +0100

    Goes through the news articles

--- index.go
+++ index.go
@@ -8,15 +8,20 @@ import (
 )
 
 func main() {
+	var s SitemapIndex
+	var n News
+
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 
-	var s SitemapIndex
 	xml.Unmarshal(bytes, &s)
 	
 	for _, Location := range s.Locations {
-		fmt.Printf("%s", Location)
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		xml.Unmarshal(bytes, &n)
 	}
 }
 
@@ -24,3 +29,8 @@ type SitemapIndex struct {
 	Locations []string `xml:"sitemap>loc"`
 }
 
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
