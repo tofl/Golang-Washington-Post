e5e7803e85dced2d71694196d0772f74d32b179e
Tom F <thomas.flitterman@ynov.com>
2018-12-25 19:21:48 +0100

    Retrieves the WPost's sitemap

--- index.go
+++ index.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
+	//"encoding/xml"
 )
 
 func main() {
-	resp, _ := http.Get()
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	stringBody := string(bytes)
+	resp.Body.Close()
+	fmt.Println(stringBody)
 }
 
