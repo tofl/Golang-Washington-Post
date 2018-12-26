2b94282f6ec8ef8c06296f66e5c23b5bfefca8ec
Tom F <thomas.flitterman@ynov.com>
2018-12-26 19:24:49 +0100

    Displays articles

--- index.go
+++ index.go
@@ -5,24 +5,45 @@ import (
 	"net/http"
 	"io/ioutil"
 	"encoding/xml"
+	"strings"
 )
 
 func main() {
 	var s SitemapIndex
 	var n News
+	newsMap := make(map[string]NewsMap)
 
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
+	defer resp.Body.Close()
 
 	xml.Unmarshal(bytes, &s)
 	
 	for _, Location := range s.Locations {
-		resp, _ := http.Get(Location)
+		resp, err := http.Get(strings.TrimSpace(Location))
+		if err != nil {
+			fmt.Println("Response :", resp)
+			fmt.Println("Error :", err)
+			return
+		}
 		bytes, _ := ioutil.ReadAll(resp.Body)
+
 		resp.Body.Close()
+
 		xml.Unmarshal(bytes, &n)
+		for idx, _ := range n.Titles {
+			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+		resp.Body.Close()
 	}
+	
+	
+	for idx, data := range newsMap {
+		fmt.Println("\n\n\n", idx)
+		fmt.Println("\n", data.Keyword)
+		fmt.Println("\n", data.Location)
+	}
+	
 }
 
 type SitemapIndex struct {
@@ -34,3 +55,9 @@ type News struct {
 	Keywords []string `xml:"url>news>keywords"`
 	Locations []string `xml:"url>loc"`
 }
+
+type NewsMap struct {
+	Keyword string
+	Location string
+}
+
